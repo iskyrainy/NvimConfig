da99bded4e24eaf55152bfd57576b24f95a7d764
iskyrainy <wangnan5117@gmail.com>
2025-09-21 23:02:34 +0800

    feat: add trouble and dap

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -10,5 +10,33 @@ return {
   },
   { "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
   { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup() end },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>.",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+  { "rafamadriz/friendly-snippets" },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+    },
+  },
+  { "mfussenegger/nvim-dap" },
 }
 
